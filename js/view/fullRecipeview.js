03d578d52143b19ff3db8fe60fcc9078c74dcb25
jonnaks <jonnaks@kth.se>
2016-02-10 08:19:36 +0100

    sista viewn å controllern. ingen css + en div funkar inte (buttondiv) :P

--- js/view/fullRecipeview.js
+++ js/view/fullRecipeview.js
@@ -1,20 +1,25 @@
 //ExampleView Object constructor
 var fullRecipe = function (container) {
-	console.log("Inne i view");
 
 	this.cnt = container;
-	this.cnt.append ('<div id="fullRecipeMain" class="col-xs-12 col-sm-12 col-md-12" style="margin: 0px;height:100%;"><p>Nu lollar vi järnet</p></div>');
+	this.cnt.append ('<div id="fullRecipeMain" class="col-xs-12 col-sm-12 col-md-12" style="margin: 0px;height:100%;"></div>');
 	$("#fullRecipeMain").hide();
+
 
 	this.show = function(menu){
 		$("#fullRecipeMain").show();
 		$("#fullRecipeMain").empty();
+		$("#fullRecipeMain").append('<div id="buttonDiv" class="col-xs-12 col-sm-12 col-md-12" style="text-align: center;"><button id="fullRecipe" type="button" class="btn btn-warning btn-responsive back" style="">Go to full recipe</button></div>');
+
 		var count = 0;
 	    var str = "";
 	        
-	    str += "<div class='table pre-scrollable' id='andra' style='margin: 0px;'>";
+	    str += "<div class='col-md-12 col-sm-12 table pre-scrollable' id='andra' style='margin: 0px;'>";
 		for(var x in menu){
-			str += "<div class='row'><div class='col-md-6 col-sm-6'>" + menu[x].name + "</div><div class='col-md-6 col-sm-6'>" + menu[x].description + "</div></div>";
+			if(menu[x] == null || menu[x] == "undefined" || menu[x] == ""){
+					continue;
+				}
+			str += "<div class='row'><image class='col-md-2 col-sm-2 img-thumbnail' src='images/"+ menu[x].image +" style='height:180px;width: 180px;'></image><div class='col-md-4 col-sm-4'>" + menu[x].name + "<br>"+menu[x].ingredients.name+"</div><div class='col-md-6 col-sm-6'>" + menu[x].description + "</div></div>";
 		}
 		str += "</div>";
 		$("#fullRecipeMain").html(str);
