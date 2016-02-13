c3cc1843fe3df97871ae4d8713af7da37ec1a294
SimonFranssonSF <fransson.simon@outlook.com>
2016-02-13 10:20:46 +0100

    försöker fixa

--- js/view/lastOverview.js
+++ js/view/lastOverview.js
@@ -5,7 +5,7 @@ var lastOverview = function (container) {
   	// and/or ones that responed to interaction)
 
 	this.cnt = container;
-	this.cnt.append ('<div id="lastOverview" class="col-xs-12 col-sm-12 col-md-12"></div>');
+	this.cnt.append('<div id="lastOverview" class="col-xs-12 col-sm-12 col-md-12" style="vertical-align: top;text-align: center;margin: 0 auto;"><div id="mainContent" class="col-md-6"><h1 id="headerContent">My dinner: people</h1></div><button id="back2" type="button" class="btn btn-warning btn-responsive back" style="">Go back and edit dinner</button></div>');
 	$("#lastOverview").hide();
 
 
@@ -32,21 +32,10 @@ this.overviewMenu = function(finalMenu, totalPrice, numGuest, prices){
 				continue;
 			}
 
-			str += '<div class="col-xs-4 col-sm-4 col-md-4" style="vertical-align: top;text-align: center;margin: 0 auto;"><div style="margin:0 auto;width: 180px;"><div style="background-color:grey;display: table;margin:0 auto;"><div style="overflow: hidden;width: 180px;"><image class="img-thumbnail " src="images/' + finalMenu[x].image + '" style="height:180px;width: 180px;"></image></div><h3>' + finalMenu[x].name + '</h3></div><p style="text-align: left;width: 180px;"> Price:' + prices[count] + '</p></div></div>';
+			str += '<div class="col-xs-3 col-sm-3 col-md-3 " style="vertical-align: top;text-align: center;margin: 0 auto;"><div style="margin:0 auto;width: 180px;"><div id="' + finalMenu[x].id + '" class="clickableDish" style="background-color:grey;display: table;margin:0 auto;"><div style="overflow: hidden;width: 180px;"><image class="img-thumbnail " src="images/' + finalMenu[x].image + '" style="height:180px;width: 180px;"></image></div><h3 class="" style="">' + finalMenu[x].name + '</h3></div><p style="text-align: left;width: 180px;">' + finalMenu[x].description + '</p></div></div>';
 			count++;
 		}
-/*
-<div row>
- 	<div 9>
- 	<div row>
- 		<div 4></div>
- 		<div 4></div>
- 		<div 4></div>
- 	</div>
- 	</div>
- 		<div 3>
- 		</div>
-</diV>*/	
+	
 		str += '</div></div><div class="col-xs-3 col-sm-3 col-md-3 " style="vertical-align: top;text-align: center;margin: 0 auto;"><p>Total Price: '+ totalPrice +'</p></div></div>';
 
 		$("#lastOverview").append(str);
