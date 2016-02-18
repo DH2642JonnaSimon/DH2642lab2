ce5b8723464a0a03d759e9414078af5051322977
jonnaks <jonnaks@kth.se>
2016-02-18 14:35:18 +0100

    test

--- js/controllers/fullRecipeController.js
+++ js/controllers/fullRecipeController.js
@@ -2,6 +2,7 @@ var fullRecipeController = function(mc, model, full_recipe_view){
 	this.mainController = mc;
 	this.dm = model;
 	this.fullRecipeview = full_recipe_view;
+	
 
 	$(document).on('click', '#back3', function(event) { 
        mc.goToEditDinner();
